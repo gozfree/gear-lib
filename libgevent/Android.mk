47d571cbe84bd9ac3254ce46c9116d6ac0f08505
gozfree <gozfree@163.com>
2015-11-08 17:19:48 +0800

    [libgevent] fix ndk-build error

--- libgevent/Android.mk
+++ libgevent/Android.mk
@@ -7,11 +7,15 @@ LOCAL_MODULE := libgevent
 LIBRARIES_DIR	:= $(LOCAL_PATH)/../
 
 LIBGZF_INC := $(LIBRARIES_DIR)/libgzf/
+LIBLOG_INC := $(LIBRARIES_DIR)/liblog/
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-                    $(LIBGZF_INC)
+                    $(LIBGZF_INC) \
+                    $(LIBLOG_INC)
 
 # Add your application source files here...
 LOCAL_SRC_FILES := libgevent.c epoll.c poll.c select.c
+
+LOCAL_LDLIBS += -llog
 
 include $(BUILD_SHARED_LIBRARY)
