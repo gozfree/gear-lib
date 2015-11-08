c999ed97b9a7f4d3944a0fb7ca4864c0b2f1a22c
gozfree <gozfree@163.com>
2015-11-08 15:49:46 +0800

    [libdict] refine and fix bug: no need to free value in key-value

--- libdict/libdict.c
+++ libdict/libdict.c
@@ -8,11 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <errno.h>
 
 #include "libdict.h"
 
 /** Minimum dictionary size to start with */
-#define DICT_MIN_SZ     8
+#define DICT_MIN_SZ     64
 
 /** Dummy pointer to reference deleted keys */
 #define DUMMY_PTR       ((void*)-1)
@@ -37,64 +39,64 @@
 /* #define dict_hash   dict_hash_dobbs */
 
 /* Forward definitions */
-static int dict_resize(dict * d);
-
+static int dict_resize(dict *d);
 
 /** Replacement for strdup() which is not always provided by libc */
-static char * xstrdup(char * s)
+static char *xstrdup(char *s)
 {
-    char * t ;
+    char * t;
     if (!s)
-        return NULL ;
-    t = malloc(strlen(s)+1) ;
+        return NULL;
+    t = malloc(strlen(s) + 1);
     if (t) {
-        strcpy(t,s);
+        strcpy(t, s);
     }
-    return t ;
+    return t;
 }
-
 
 /**
   This hash function has been taken from an Article in Dr Dobbs Journal.
   There are probably better ones out there but this one does the job.
  */
 #if 0
-static unsigned dict_hash_dobbs(char * key)
+static uint32_t dict_hash_dobbs(char *key, size_t len)
 {
-    int         len ;
-    unsigned    hash ;
-    int         i ;
-
-    len = strlen(key);
-    for (hash=0, i=0 ; i<len ; i++) {
-        hash += (unsigned)key[i] ;
+    int len, i;
+    uint32_t hash;
+    if (!key || !len) {
+        printf("%s: invalid paraments!\n", __func__);
+        return 0;
+    }
+    for (hash = 0, i=0; i < len; i++) {
+        hash += (uint32_t)key[i];
         hash += (hash<<10);
-        hash ^= (hash>>6) ;
+        hash ^= (hash>>6);
     }
     hash += (hash <<3);
     hash ^= (hash >>11);
     hash += (hash <<15);
-    return hash ;
+    return hash;
 }
 #endif
 
-
 /* Murmurhash */
-static unsigned dict_hash_murmur(char * key)
+static uint32_t dict_hash_murmur(char *key, size_t len)
 {
-    int         len ;
-    unsigned    h, k, seed ;
-    unsigned    m = 0x5bd1e995 ;
-    int         r = 24 ;
-    unsigned char * data ;
+    uint32_t h, k;
+    uint8_t *data;
+    uint32_t seed = 0x0badcafe;//magic number
+    uint32_t m = 0x5bd1e995;//magic number
+    uint32_t r = 24;//magic number
 
-    seed = 0x0badcafe ;
-    len  = (int)strlen(key);
+    if (!key || !len) {
+        printf("%s: invalid paraments!\n", __func__);
+        return 0;
+    }
 
-    h = seed ^ len ;
-    data = (unsigned char *)key ;
-    while(len >= 4) {
-        k = *(unsigned int *)data;
+    h = seed ^ len;
+    data = (uint8_t *)key;
+    while (len >= 4) {
+        k = *(uint32_t *)data;
 
         k *= m;
         k ^= k >> r;
@@ -106,17 +108,15 @@ static unsigned dict_hash_murmur(char * key)
         data += 4;
         len -= 4;
     }
-    switch(len) {
+    switch (len) {
         case 3: h ^= data[2] << 16;
         case 2: h ^= data[1] << 8;
         case 1: h ^= data[0];
                 h *= m;
     };
-
     h ^= h >> 13;
     h *= m;
     h ^= h >> 15;
-
     return h;
 }
 
@@ -124,76 +124,80 @@ static unsigned dict_hash_murmur(char * key)
     This implementation copied almost verbatim from the Python dictionary
     object, without the Pythonisms.
     */
-static keypair * dict_lookup(dict * d, char * key, unsigned hash)
+static keypair *dict_lookup(dict *d, char *key, uint32_t hash)
 {
-    keypair * freeslot ;
-    keypair * ep ;
-    unsigned    i ;
-    unsigned    perturb ;
+    keypair * freeslot;
+    keypair * ep;
+    uint32_t i;
+    uint32_t perturb;
 
-    if (!d || !key)
-        return NULL ;
+    if (!d || !key) {
+        printf("%s: invalid paraments!\n", __func__);
+        return NULL;
+    }
 
-    i = hash & (d->size-1) ;
+    i = hash & (d->size-1);
     /* Look for empty slot */
-    ep = d->table + i ;
-    if (ep->key==NULL || ep->key==key) {
+    ep = d->table + i;
+    if (ep->key == NULL || ep->key == key) {
         return ep ;
     }
     if (ep->key == DUMMY_PTR) {
-        freeslot = ep ;
+        freeslot = ep;
     } else {
         if (ep->hash == hash &&
             !strcmp(key, ep->key)) {
-                return ep ;
+                return ep;
         }
-        freeslot = NULL ;
+        freeslot = NULL;
     }
-    for (perturb=hash ; ; perturb >>= PERTURB_SHIFT) {
+    for (perturb = hash; ; perturb >>= PERTURB_SHIFT) {
         i = (i<<2) + i + perturb + 1;
         i &= (d->size-1);
         ep = d->table + i;
-        if (ep->key==NULL) {
-            return freeslot==NULL ? ep : freeslot ;
+        if (ep->key == NULL) {
+            return freeslot == NULL ? ep : freeslot;
         }
         if ((ep->key == key) ||
             (ep->hash == hash &&
              ep->key  != DUMMY_PTR &&
              !strcmp(ep->key, key))) {
-            return ep ;
+            return ep;
         }
-        if (ep->key==DUMMY_PTR && freeslot==NULL) {
-            freeslot = ep ;
+        if (ep->key == DUMMY_PTR && freeslot == NULL) {
+            freeslot = ep;
         }
     }
-    return NULL ;
+    return NULL;
 }
 
 /** Add an item to a dictionary without copying key/val
     Used by dict_resize() only.
  */
-static int dict_add_p(dict * d, char * key, char * val)
+static int dict_add_p(dict *d, char *key, char *val)
 {
-    unsigned  hash ;
-    keypair * slot ;
+    uint32_t hash;
+    keypair * slot;
 
-    if (!d || !key)
-        return -1 ;
+    if (!d || !key) {
+        printf("%s: invalid paraments!\n", __func__);
+        return -1;
+    }
 
 #if DEBUG>2
     printf("dict_add_p[%s][%s]\n", key, val ? val : "UNDEF");
 #endif
-    hash = dict_hash(key);
+    hash = dict_hash(key, strlen(key));
     slot = dict_lookup(d, key, hash);
     if (slot) {
-        slot->key  = key;
-        slot->val  = val ;
-        slot->hash = hash ;
-        d->used++ ;
-        d->fill++ ;
+        slot->key = key;
+        slot->val = val;
+        slot->hash = hash;
+        d->used++;
+        d->fill++;
         if ((3*d->fill) >= (d->size*2)) {
-            if (dict_resize(d)!=0) {
-                return -1 ;
+            if (dict_resize(d) != 0) {
+                return -1;
             }
         }
     }
@@ -201,36 +205,40 @@ static int dict_add_p(dict * d, char * key, char * val)
 }
 
 /** Add an item to a dictionary by copying key/val into the dict. */
-int dict_add(dict * d, char * key, char * val)
+int dict_add(dict *d, char *key, char *val)
 {
-    unsigned  hash ;
-    keypair * slot ;
+    uint32_t hash;
+    keypair *slot;
 
-    if (!d || !key)
-        return -1 ;
+    if (!d || !key) {
+        printf("%s: invalid paraments!\n", __func__);
+        return -1;
+    }
 
 #if DEBUG>2
     printf("dict_add[%s][%s]\n", key, val ? val : "UNDEF");
 #endif
-    hash = dict_hash(key);
+    hash = dict_hash(key, strlen(key));
     slot = dict_lookup(d, key, hash);
     if (slot) {
-        slot->key  = xstrdup(key);
+        slot->key = xstrdup(key);
         if (!(slot->key)) {
-            return -1 ;
+            return -1;
         }
-        //slot->val  = val ? xstrdup(val) : val ;
-        slot->val  = val;
+#if 0
+        slot->val = val ? xstrdup(val) : val;
         if (val && !(slot->val)) {
             free(slot->key);
-            return -1 ;
+            return -1;
         }
-        slot->hash = hash ;
-        d->used++ ;
-        d->fill++ ;
+#endif
+        slot->val = val;
+        slot->hash = hash;
+        d->used++;
+        d->fill++;
         if ((3*d->fill) >= (d->size*2)) {
-            if (dict_resize(d)!=0) {
-                return -1 ;
+            if (dict_resize(d) != 0) {
+                return -1;
             }
         }
     }
@@ -238,41 +246,42 @@ int dict_add(dict * d, char * key, char * val)
 }
 
 /** Resize a dictionary */
-static int dict_resize(dict * d)
+static int dict_resize(dict *d)
 {
-    unsigned      newsize ;
-    keypair *   oldtable ;
-    unsigned      i ;
-    unsigned      oldsize ;
-    unsigned      factor ;
+    uint32_t newsize;
+    keypair *oldtable;
+    uint32_t i;
+    uint32_t oldsize;
+    uint32_t factor;
 
-    newsize = d->size ;
+    newsize = d->size;
     /*
      * Re-sizing factor depends on the current dict size.
      * Small dicts will expand 4 times, bigger ones only 2 times
      */
-    factor = (d->size>DICT_BIGSZ) ? 2 : 4 ;
-    while (newsize<=(factor*d->used)) {
-        newsize*=2 ;
+    factor = (d->size>DICT_BIGSZ) ? 2 : 4;
+    while (newsize <= (factor*d->used)) {
+        newsize *= 2;
     }
     /* Exit early if no re-sizing needed */
-    if (newsize==d->size)
-        return 0 ;
+    if (newsize == d->size)
+        return 0;
 #if DEBUG>2
     printf("resizing %d to %d (used: %d)\n", d->size, newsize, d->used);
 #endif
     /* Shuffle pointers, re-allocate new table, re-insert data */
-    oldtable = d->table ;
+    oldtable = d->table;
     d->table = calloc(newsize, sizeof(keypair));
     if (!(d->table)) {
         /* Memory allocation failure */
-        return -1 ;
+        printf("%s: malloc failed %s\n", __func__, strerror(errno));
+        return -1;
     }
-    oldsize  = d->size ;
-    d->size  = newsize ;
-    d->used  = 0 ;
-    d->fill  = 0 ;
-    for (i=0 ; i<oldsize ; i++) {
+    oldsize = d->size;
+    d->size = newsize;
+    d->used = 0;
+    d->fill = 0;
+    for (i = 0; i < oldsize; i++) {
         if (oldtable[i].key && (oldtable[i].key!=DUMMY_PTR)) {
             dict_add_p(d, oldtable[i].key, oldtable[i].val);
         }
@@ -281,35 +290,41 @@ static int dict_resize(dict * d)
     return 0 ;
 }
 
-
 /** Public: allocate a new dict */
-dict * dict_new(void)
+dict *dict_new(void)
 {
-    dict * d ;
-
-    d = calloc(1, sizeof(dict));
-    if (!d)
+    dict * d = calloc(1, sizeof(dict));
+    if (!d) {
+        printf("%s: malloc failed %s\n", __func__, strerror(errno));
         return NULL;
-    d->size  = DICT_MIN_SZ ;
-    d->used  = 0 ;
-    d->fill  = 0 ;
+    }
+    d->size = DICT_MIN_SZ;
+    d->used = 0;
+    d->fill = 0;
     d->table = calloc(DICT_MIN_SZ, sizeof(keypair));
-    return d ;
+    if (!d->table) {
+        printf("%s: malloc failed %s\n", __func__, strerror(errno));
+        free(d);
+        return NULL;
+    }
+    return d;
 }
 
 /** Public: deallocate a dict */
-void dict_free(dict * d)
+void dict_free(dict *d)
 {
-    int i ;
-    
+    int i;
     if (!d)
-        return ;
+        return;
 
-    for (i=0 ; i<d->size ; i++) {
-        if (d->table[i].key && d->table[i].key!=DUMMY_PTR) {
+    for (i = 0; i < d->size; i++) {
+        if (d->table[i].key && d->table[i].key != DUMMY_PTR) {
             free(d->table[i].key);
+#if 0
+//val is not copyed, no need to free
             if (d->table[i].val)
                 free(d->table[i].val);
+#endif
         }
     }
     free(d->table);
@@ -318,95 +333,106 @@ void dict_free(dict * d)
 }
 
 /** Public: get an item from a dict */
-char * dict_get(dict * d, char * key, char * defval)
+char *dict_get(dict *d, char *key, char *defval)
 {
-    keypair * kp ;
-    unsigned  hash ;
+    keypair *kp;
+    uint32_t hash;
 
-    if (!d || !key)
-        return defval ;
+    if (!d || !key) {
+        printf("%s: invalid paraments!\n", __func__);
+        return defval;
+    }
 
-    hash = dict_hash(key);
+    hash = dict_hash(key, strlen(key));
     kp = dict_lookup(d, key, hash);
     if (kp) {
-        return kp->val ;
+        return kp->val;
     }
     return defval;
 }
 
 /** Public: delete an item in a dict */
-int dict_del(dict * d, char * key)
+int dict_del(dict *d, char *key)
 {
-    unsigned    hash ;
-    keypair *   kp ;
+    uint32_t hash;
+    keypair *kp;
 
-    if (!d || !key)
-        return -1 ;
+    if (!d || !key) {
+        printf("%s: invalid paraments!\n", __func__);
+        return -1;
+    }
 
-    hash = dict_hash(key);
+    hash = dict_hash(key, strlen(key));
     kp = dict_lookup(d, key, hash);
     if (!kp)
-        return -1 ;
-    if (kp->key && kp->key!=DUMMY_PTR)
+        return -1;
+    if (kp->key && kp->key != DUMMY_PTR)
         free(kp->key);
     kp->key = DUMMY_PTR;
+#if 0
     if (kp->val)
         free(kp->val);
-    kp->val = NULL ;
-    d->used -- ;
-    return 0 ;
+#endif
+    kp->val = NULL;
+    d->used--;
+    return 0;
 }
 
 /** Public: enumerate a dictionary */
 int dict_enumerate(dict * d, int rank, char ** key, char ** val)
 {
-    if (!d || !key || !val || (rank<0))
+    if (!d || !key || !val || (rank<0)) {
+        printf("%s: invalid paraments!\n", __func__);
         return -1 ;
+    }
 
-    while ((d->table[rank].key==NULL || d->table[rank].key==DUMMY_PTR) &&
-           (rank<d->size))
+    while ((d->table[rank].key == NULL
+            || d->table[rank].key == DUMMY_PTR)
+            && (rank<d->size))
         rank++;
 
     if (rank>=d->size) {
         *key = NULL;
         *val = NULL;
-        rank = -1 ;
+        rank = -1;
     } else {
-        *key = d->table[rank].key ;
-        *val = d->table[rank].val ;
+        *key = d->table[rank].key;
+        *val = d->table[rank].val;
         rank++;
     }
-    return rank ;
+    return rank;
 }
 
 /** Public: dump a dict to a file pointer */
 void dict_dump(dict * d, FILE * out)
 {
-    char * key ;
-    char * val ;
-    int    rank=0 ;
+    char *key;
+    char *val;
+    int rank = 0;
 
-    if (!d || !out)
-        return ;
+    if (!d || !out) {
+        printf("%s: invalid paraments!\n", __func__);
+        return;
+    }
 
     while (1) {
         rank = dict_enumerate(d, rank, &key, &val);
-        if (rank<0)
-            break ;
+        if (rank < 0)
+            break;
         fprintf(out, "[rank=%d] %20s: %s\n", rank, key, val ? val : "UNDEF");
     }
-    return ;
+    return;
 }
 
-void dict_get_key_list(dict * d, key_list **klist)
+void dict_get_key_list(dict *d, key_list **klist)
 {
-    char * key ;
-    char * val ;
-    int    rank = 0;
+    char *key;
+    char *val;
+    int rank = 0;
     key_list *knode, *ktmp;
 
     if (!d)
-        return ;
+        return;
 
     *klist = NULL;
     knode = NULL;
--- libdict/test_libdict.c
+++ libdict/test_libdict.c
@@ -10,9 +10,9 @@
 #include <sys/time.h>
 #include "libdict.h"
 
-#define ALIGN   "%15s: %6.4f\n"
-//#define NKEYS   1024*1024
-#define NKEYS   10
+#define ALIGN   "%15s: %6.4f sec\n"
+#define NKEYS   1024*1024
+//#define NKEYS   10
 
 double epoch_double()
 {
@@ -23,20 +23,24 @@ double epoch_double()
 
 int main(int argc, char * argv[])
 {
-    dict * d ;
-    double t1, t2 ;
-    int i ;
-    int nkeys ;
-    char * buffer ;
-    char * val ;
+    dict * d;
+    double t1, t2;
+    int i;
+    int nkeys;
+    char * buffer;
+    char * val;
 
-    nkeys = (argc>1) ? (int)atoi(argv[1]) : NKEYS ;
+    nkeys = (argc>1) ? (int)atoi(argv[1]) : NKEYS;
     printf("%15s: %d\n", "values", nkeys);
     buffer = malloc(9 * nkeys);
+    if (!buffer) {
+        printf("malloc failed!\n");
+        return -1;
+    }
 
     d = dict_new();
     t1 = epoch_double();
-    for(i = 0; i < nkeys; i++) {
+    for (i = 0; i < nkeys; i++) {
         sprintf(buffer + i * 9, "%08x", i);
     }
     t2 = epoch_double();
@@ -45,17 +49,18 @@ int main(int argc, char * argv[])
     t1 = epoch_double();
     for(i = 0; i < nkeys; i++) {
         dict_add(d, buffer + i*9, buffer +i*9);
-        printf("hash_set: key=%p, val=%p\n", buffer + i*9, buffer + i*9);
+        //printf("hash_set: key=%p, val=%p\n", buffer + i*9, buffer + i*9);
     }
-    dict_dump(d, stdout);
+    //dict_dump(d, stdout);
     t2 = epoch_double();
     printf(ALIGN, "adding", t2 - t1);
 
     t1 = epoch_double();
     for(i = 0; i < nkeys; i++) {
         val = dict_get(d, buffer + i*9, "UNDEF");
+        if (0) {
         printf("hash_get: key=%p, val=%p\n", buffer + i*9, val);
-//#define DEBUG           3
+        }
 #if DEBUG>1
         printf("exp[%s] got[%s]\n", buffer+i*9, val);
         if (val && strcmp(val, buffer+i*9)) {
@@ -66,11 +71,12 @@ int main(int argc, char * argv[])
     t2 = epoch_double();
     printf(ALIGN, "lookup", t2 - t1);
 
-    if (nkeys<100)
-        dict_dump(d, stdout);
+//    if (nkeys<100)
+//        dict_dump(d, stdout);
 
     t1 = epoch_double();
     for(i = 0; i < nkeys; i++) {
+        //printf("dict_del %d, string = %s\n", i, buffer+i*9);
         dict_del(d, buffer + i*9);
     }
     t2 = epoch_double();
@@ -89,6 +95,5 @@ int main(int argc, char * argv[])
     printf(ALIGN, "free", t2 - t1);
 
     free(buffer);
-    return 0 ;
-
+    return 0;
 }
