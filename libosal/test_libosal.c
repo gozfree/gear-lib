409383e7194889f4d81c0ef7f87d9dc39d147024
gozfree <gozfree@163.com>
2016-09-27 11:07:07 +0800

    1. remove libfile/core 2. fix libosal compile error

--- libosal/test_libosal.c
+++ libosal/test_libosal.c
@@ -7,7 +7,7 @@
  *****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <libosal.h>
+#include "libosal.h"
 
 int main(int argc, char **argv)
 {
