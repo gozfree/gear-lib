b264d921e1a192abdbc7a6248cd9194f10ea7a6a
gozfree <gozfree@163.com>
2018-01-24 17:49:05 +0800

    [libmacro] update license

--- libmacro/libmacro.h
+++ libmacro/libmacro.h
@@ -1,10 +1,20 @@
 /******************************************************************************
- * Copyright (C) 2014-2015
- * file:    libmacro.h
- * author:  gozfree <gozfree@163.com>
- * created: 2016-06-29 11:07:41
- * updated: 2016-06-29 11:07:41
- *****************************************************************************/
+ * Copyright (C) 2014-2018 Zhifeng Gong <gozfree@163.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libraries; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ ******************************************************************************/
 #ifndef LIBMACRO_H
 #define LIBMACRO_H
 
@@ -29,8 +39,8 @@ extern "C" {
 #define SWAP(a, b)          \
     do { typeof(a) __tmp = (a); (a) = (b); (b) = __tmp; } while (0)
 
-#define MIN(a, b)           ((a) > (b) ? (b) : (a))
-#define MAX(a, b)           ((a) > (b) ? (a) : (b))
+#define MIN2(a, b)           ((a) > (b) ? (b) : (a))
+#define MAX2(a, b)           ((a) > (b) ? (a) : (b))
 
 #define CALLOC(size, type)  (type *)calloc(size, sizeof(type))
 #define SIZEOF(array)       (sizeof(array)/sizeof(array[0]))
@@ -54,6 +64,7 @@ extern "C" {
         printf("\n");                                           \
     } while (0)
 
+#define ALIGN(x, a)	(((x) + (a) - 1) & ~((a) - 1))
 
 
 
