d36d73feed4df734669b5cd1aaf0cc79832debec
gozfree <gozfree@163.com>
2019-01-29 00:53:55 +0800

    [libthread] refine atomic API

--- libthread/libatomic.c
+++ libthread/libatomic.c
@@ -21,48 +21,120 @@
  ******************************************************************************/
 #include "libatomic.h"
 
-#define HAVE_PTHREADS   1
+#define GCC_VERSION (__GNUC__*100 + __GNUC_MINOR__*10 + __GNUC_PATCHLEVEL__)
 
-#if !HAVE_ATOMICS_NATIVE
+#if GCC_VERSION > 463
+#define HAVE_ATOMIC_COMPARE_EXCHANGE 1
+#else
+#define HAVE_ATOMIC_COMPARE_EXCHANGE 0
+#endif
 
-#if HAVE_PTHREADS
+#define HAVE_ATOMICS_NATIVE 1
+#define HAVE_SYNC_VAL_COMPARE_AND_SWAP 1
 
+#if HAVE_THREADS
 #include <pthread.h>
-
 static pthread_mutex_t atomic_lock = PTHREAD_MUTEX_INITIALIZER;
+#endif
 
-int atomic_int_get(volatile int *ptr)
+inline int atomic_int_get(volatile int *ptr)
 {
+#if HAVE_ATOMICS_NATIVE
+  #if HAVE_ATOMIC_COMPARE_EXCHANGE
+    return __atomic_load_n(ptr, __ATOMIC_SEQ_CST);
+  #else
+    __sync_synchronize();
+    return *ptr;
+  #endif
+#elif HAVE_THREADS
     int res;
-
     pthread_mutex_lock(&atomic_lock);
     res = *ptr;
     pthread_mutex_unlock(&atomic_lock);
-
     return res;
+#else
+    return *ptr;
+#endif
+
 }
 
-void atomic_int_set(volatile int *ptr, int val)
+inline void atomic_int_set(volatile int *ptr, int val)
 {
+#if HAVE_ATOMICS_NATIVE
+  #if HAVE_ATOMIC_COMPARE_EXCHANGE
+    __atomic_store_n(ptr, val, __ATOMIC_SEQ_CST);
+  #else
+    *ptr = val;
+    __sync_synchronize();
+  #endif
+#elif HAVE_THREADS
     pthread_mutex_lock(&atomic_lock);
     *ptr = val;
     pthread_mutex_unlock(&atomic_lock);
+#else
+    *ptr = val;
+#endif
 }
 
-int atomic_int_add_and_fetch(volatile int *ptr, int inc)
+inline int atomic_int_add_and_fetch(volatile int *ptr, int inc)
 {
+#if HAVE_ATOMICS_NATIVE
+  #if HAVE_ATOMIC_COMPARE_EXCHANGE
+    return __atomic_add_fetch(ptr, inc, __ATOMIC_SEQ_CST);
+  #else
+    return __sync_add_and_fetch(ptr, inc);
+  #endif
+#elif HAVE_THREADS
     int res;
-
     pthread_mutex_lock(&atomic_lock);
     *ptr += inc;
     res = *ptr;
     pthread_mutex_unlock(&atomic_lock);
-
     return res;
+#else
+    *ptr += inc;
+    return *ptr;
+#endif
 }
 
-void *atomic_ptr_cas(void * volatile *ptr, void *oldval, void *newval)
+inline int atomic_int_sub_and_fetch(volatile int *ptr, int inc)
 {
+#if HAVE_ATOMICS_NATIVE
+  #if HAVE_ATOMIC_COMPARE_EXCHANGE
+    return __atomic_sub_fetch(ptr, inc, __ATOMIC_SEQ_CST);
+  #else
+    return __sync_sub_and_fetch(ptr, inc);
+  #endif
+#elif HAVE_THREADS
+    int res;
+    pthread_mutex_lock(&atomic_lock);
+    *ptr -= inc;
+    res = *ptr;
+    pthread_mutex_unlock(&atomic_lock);
+    return res;
+#else
+    *ptr -= inc;
+    return *ptr;
+#endif
+}
+
+inline void *atomic_ptr_cas(void * volatile *ptr,
+                            void *oldval, void *newval)
+{
+#if HAVE_ATOMICS_NATIVE
+  #if HAVE_SYNC_VAL_COMPARE_AND_SWAP
+    #ifdef __ARMCC_VERSION
+    // armcc will throw an error if ptr is not an integer type
+    volatile uintptr_t *tmp = (volatile uintptr_t*)ptr;
+    return (void*)__sync_val_compare_and_swap(tmp, oldval, newval);
+    #else
+    return __sync_val_compare_and_swap(ptr, oldval, newval);
+    #endif
+  #else
+    __atomic_compare_exchange_n(ptr, &oldval, newval, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+    return oldval;
+  #endif
+#elif HAVE_THREADS
     void *ret;
     pthread_mutex_lock(&atomic_lock);
     ret = *ptr;
@@ -70,55 +142,21 @@ void *atomic_ptr_cas(void * volatile *ptr, void *oldval, void *newval)
         *ptr = newval;
     pthread_mutex_unlock(&atomic_lock);
     return ret;
-}
-
-
-
-#elif !HAVE_THREADS
-
-int atomic_int_get(volatile int *ptr)
-{
-    return *ptr;
-}
-
-void atomic_int_set(volatile int *ptr, int val)
-{
-    *ptr = val;
-}
-
-int atomic_int_add_and_fetch(volatile int *ptr, int inc)
-{
-    *ptr += inc;
-    return *ptr;
-}
-
-void *atomic_ptr_cas(void * volatile *ptr, void *oldval, void *newval)
-{
+#else
     if (*ptr == oldval) {
         *ptr = newval;
         return oldval;
     }
     return *ptr;
+#endif
 }
 
-#else /* HAVE_THREADS */
-
-/* This should never trigger, unless a new threading implementation
- * without correct atomics dependencies in configure or a corresponding
- * atomics implementation is added. */
-#error "Threading is enabled, but there is no implementation of atomic operations available"
-
-#endif /* HAVE_PTHREADS */
-
-int atomic_int_inc(volatile int *ptr)
+inline int atomic_int_inc(volatile int *ptr)
 {
     return atomic_int_add_and_fetch(ptr, 1);
 }
 
-int atomic_int_dec(volatile int *ptr)
+inline int atomic_int_dec(volatile int *ptr)
 {
     return atomic_int_add_and_fetch(ptr, -1);
 }
-#endif /* !HAVE_ATOMICS_NATIVE */
-
-
--- libthread/libatomic.h
+++ libthread/libatomic.h
@@ -26,105 +26,6 @@
 extern "C" {
 #endif
 
-#define HAVE_ATOMICS_NATIVE 1
-#define HAVE_ATOMICS_GCC    1
-
-#if HAVE_ATOMICS_NATIVE
-
-#if HAVE_ATOMICS_GCC
-#define GCC_VERSION (__GNUC__*100 + __GNUC_MINOR__*10 + __GNUC_PATCHLEVEL__)
-
-#if GCC_VERSION > 463
-#define HAVE_ATOMIC_COMPARE_EXCHANGE 1
-#else
-#define HAVE_ATOMIC_COMPARE_EXCHANGE 0
-#endif
-
-#define HAVE_SYNC_VAL_COMPARE_AND_SWAP 1
-
-#define atomic_int_get atomic_int_get_gcc
-static inline int atomic_int_get_gcc(volatile int *ptr)
-{
-#if HAVE_ATOMIC_COMPARE_EXCHANGE
-    return __atomic_load_n(ptr, __ATOMIC_SEQ_CST);
-#else
-    __sync_synchronize();
-    return *ptr;
-#endif
-}
-
-#define atomic_int_set atomic_int_set_gcc
-static inline void atomic_int_set_gcc(volatile int *ptr, int val)
-{
-#if HAVE_ATOMIC_COMPARE_EXCHANGE
-    __atomic_store_n(ptr, val, __ATOMIC_SEQ_CST);
-#else
-    *ptr = val;
-    __sync_synchronize();
-#endif
-}
-
-#define atomic_int_add_and_fetch atomic_int_add_and_fetch_gcc
-static inline int atomic_int_add_and_fetch_gcc(volatile int *ptr, int inc)
-{
-#if HAVE_ATOMIC_COMPARE_EXCHANGE
-    return __atomic_add_fetch(ptr, inc, __ATOMIC_SEQ_CST);
-#else
-    return __sync_add_and_fetch(ptr, inc);
-#endif
-}
-
-#define atomic_int_sub_and_fetch atomic_int_sub_and_fetch_gcc
-static inline int atomic_int_sub_and_fetch_gcc(volatile int *ptr, int inc)
-{
-#if HAVE_ATOMIC_COMPARE_EXCHANGE
-    return __atomic_sub_fetch(ptr, inc, __ATOMIC_SEQ_CST);
-#else
-    return __sync_sub_and_fetch(ptr, inc);
-#endif
-}
-
-#define atomic_int_inc atomic_int_inc_gcc
-static inline int atomic_int_inc_gcc(volatile int *ptr)
-{
-    return atomic_int_add_and_fetch(ptr, 1);
-}
-
-#define atomic_int_dec atomic_int_dec_gcc
-static inline int atomic_int_dec_gcc(volatile int *ptr)
-{
-    return atomic_int_add_and_fetch(ptr, -1);
-}
-
-
-
-#define atomic_ptr_cas atomic_ptr_cas_gcc
-static inline void *atomic_ptr_cas_gcc(void * volatile *ptr,
-                                       void *oldval, void *newval)
-{
-#if HAVE_SYNC_VAL_COMPARE_AND_SWAP
-#ifdef __ARMCC_VERSION
-    // armcc will throw an error if ptr is not an integer type
-    volatile uintptr_t *tmp = (volatile uintptr_t*)ptr;
-    return (void*)__sync_val_compare_and_swap(tmp, oldval, newval);
-#else
-    return __sync_val_compare_and_swap(ptr, oldval, newval);
-#endif
-#else
-    __atomic_compare_exchange_n(ptr, &oldval, newval, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
-    return oldval;
-#endif
-}
-
-
-#elif HAVE_ATOMICS_WIN32
-//#include "libatomic_win32.h"
-#elif HAVE_ATOMICS_SUNCC
-//#include "libatomic_suncc.h"
-#endif
-
-#else
-
 /**
  * Load the current value stored in an atomic integer.
  *
@@ -154,9 +55,7 @@ void atomic_int_set(volatile int *ptr, int val);
  */
 int atomic_int_add_and_fetch(volatile int *ptr, int inc);
 int atomic_int_sub_and_fetch(volatile int *ptr, int inc);
-
 int atomic_int_inc(volatile int *ptr);
-
 int atomic_int_dec(volatile int *ptr);
 
 /**
@@ -168,8 +67,6 @@ int atomic_int_dec(volatile int *ptr);
  * @return the value of *ptr before comparison
  */
 void *atomic_ptr_cas(void * volatile *ptr, void *oldval, void *newval);
-
-#endif /* HAVE_ATOMICS_NATIVE */
 
 
 #ifdef __cplusplus
