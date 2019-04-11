155f4b14f6eb1294fca956eb5ad36fbe70223954
Lei <lynnl.yet@gmail.com>
2019-04-11 22:43:21 +0800

    PATCH libmacro: Support over BSD-derived systems
    BSD macro is defined in most BSD-derived systems,
    
    Notably like macOS, FreeBSD, OpenBSD, NetBSD, DragonFlyBSD, etc.
    
    Also the header <sys/uio.h> is available in those BSD systems
    
    see:
    https://sourceforge.net/p/predef/wiki/OperatingSystems/

--- libmacro/libmacro.h
+++ libmacro/libmacro.h
@@ -25,7 +25,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#if defined (__linux__) || defined (__CYGWIN__)
+#if !(defined(__WIN32__) || defined(_WIN32) || defined(_MSC_VER))
+#include <sys/param.h>
+#endif
+
+#if defined (__linux__) || defined (__CYGWIN__) || defined (BSD)
 #include <stdbool.h>
 #include <sys/uio.h>
 #elif defined (__WIN32__) || defined (WIN32) || defined (_MSC_VER)
