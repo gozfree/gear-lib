92c0a1989c75a8a7d327bbcac983d4bcf36d6fc9
gozfree <gozfree@163.com>
2019-07-07 15:53:39 +0800

    [libposix4win] update pthread_cond_timedwait

--- libposix4win/libposix4win.c
+++ libposix4win/libposix4win.c
@@ -195,6 +195,13 @@ int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex)
     return 0;
 }
 
+int pthread_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, int ms)
+{
+    SleepConditionVariableSRW(cond, (PSRWLOCK)mutex, ms, 0);
+    return 0;
+}
+
+
 int pthread_cond_signal(pthread_cond_t *cond)
 {
     WakeConditionVariable(cond);
--- libposix4win/libposix4win.h
+++ libposix4win/libposix4win.h
@@ -158,6 +158,7 @@ int pthread_cond_init(pthread_cond_t *cond, const void *unused_attr);
 int pthread_cond_destroy(pthread_cond_t *cond);
 int pthread_cond_broadcast(pthread_cond_t *cond);
 int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex);
+int pthread_cond_timedwait(pthread_cond_t *cond, pthread_mutex_t *mutex, int ms);
 int pthread_cond_signal(pthread_cond_t *cond);
 
 #define getpid                    GetCurrentProcessId
