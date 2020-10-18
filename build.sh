68bd6ebb42a161cfcd92b222bff7f06dcaf8e279
gozfree <gozfree@163.com>
2020-10-18 17:08:52 +0800

    up

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ PLATFORM="[linux|pi|android|ios]"
 
 #basic libraries
 BASIC_LIBS="libposix libtime liblog libdarray libgevent libworkq libdict libhash libsort \
-	    librbtree libringbuffer libthread libvector libbase64 libmedia-io \
+	    librbtree libringbuffer libthread libvector libbase64 libmedia-io libmp4 \
             libdebug libfile libconfig libuac libuvc libqueue libplugin libhal libsubmask"
 FRAMEWORK_LIBS="libipc"
 NETWORK_LIBS="libskt librpc librtsp librtmpc"
