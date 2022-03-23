a197443f46e1f9f723a633fff338f2d6d1fd2bb7
gozfree <gozfree@163.com>
2022-03-23 21:39:50 +0800

    fix compile on linux

--- gear-lib/libbase64/test_libbase64.c
+++ gear-lib/libbase64/test_libbase64.c
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
     memset(target2, 0, sizeof(target2));
     ret_bytes = base64_encode(target, source, strlen(source));
 
-    printf("src size: %Iu , return byte: %d , target:%s\n", strlen(source), ret_bytes, target);
+    printf("src size: %zu , return byte: %d , target:%s\n", strlen(source), ret_bytes, target);
 
     ret_bytes = base64_decode(target2, target, ret_bytes);
     target[ret_bytes]='\0';
